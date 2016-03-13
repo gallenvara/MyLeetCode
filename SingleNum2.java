875fa7cc415502746c51464de9deebc0abde26ad
Lun Gao <gallenvara@126.com>
2016-03-13 15:07:32 +0800

    2016-3-13

---
+++ SingleNum2.java
@@ -0,0 +1,75 @@
+/*
+Given an array of integers, every element appears three times except for one. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+public class Solution {
+    public static int table_size = 10000;
+    public int singleNumber(int[] nums) {
+        int[] hash = new int[table_size];
+        int[] b = new int[10000];
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            while (temp < 0) {
+                temp += table_size;
+            }
+            hash[temp % table_size]++;
+        }
+        int find = 0;
+        int index = 0;
+        for (int i = 0; i < table_size; i++) {
+            if (hash[i] % 3 == 1) find = i;
+        }
+        
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            while (temp < 0) {
+                temp += table_size;
+            }
+            if (temp % table_size == find) {
+                b[index++] = nums[i];
+            }
+        }
+        if (index == 1) return b[0];
+        quickSort(b, 0, index - 1);
+        if (b[0] != b[1]) return b[0];
+        if (b[index - 1] != b[index - 2]) return b[index - 1];
+        for (int i = 1; i < index - 1; i++) {
+            if (((b[i] != b[i - 1])&&(b[i] != b[i + 1]))) {
+                return b[i];
+            }
+        }
+        return 0;
+    }
+    private void quickSort(int[] array, int lowerIndex, int higherIndex) {
+         
+        int i = lowerIndex;
+        int j = higherIndex;
+        int pivot = array[(lowerIndex + higherIndex) >> 1];
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(array, i, j);                
+                i++;
+                j--;
+            }
+        }
+        if (lowerIndex < j)
+            quickSort(array, lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(array, i, higherIndex);
+    }
+ 
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
