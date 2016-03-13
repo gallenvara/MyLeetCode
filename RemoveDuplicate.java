6efa951e2adaeb28335ac4ed2040a22a29cb7ddf
Lun Gao <gallenvara@126.com>
2016-03-13 21:19:11 +0800

    2016-3-13

---
+++ RemoveDuplicate.java
@@ -0,0 +1,49 @@
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+*/
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) return nums[i];
+        }
+        return 0;
+    }
+    private void quickSort(int[] array, int lowerIndex, int higherIndex) {
+         
+        int i = lowerIndex;
+        int j = higherIndex;
+        int pivot = array[(lowerIndex + higherIndex) >> 1];
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(array, i, j);                
+                i++;
+                j--;
+            }
+        }
+        if (lowerIndex < j)
+            quickSort(array, lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(array, i, higherIndex);
+    }
+ 
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
