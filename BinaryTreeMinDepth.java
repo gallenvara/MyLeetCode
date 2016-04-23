b90ef733c1a761d9b8bfaf19c9cabae90407c9cc
gallenvara <gallenvara@126.com>
2016-04-23 22:00:49 +0800

    modify min depth

--- BinaryTreeMinDepth.java
+++ BinaryTreeMinDepth.java
@@ -39,3 +39,31 @@ public class Solution {
         return x > y ? y : x;
     }
 }
+//写复杂了，可以化简一下，这里只需要注意求最小深度时需要对单孩子节点处理一下；
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        return minFunc(minDepth(root.left),minDepth(root.right)) + 1;
+    }
+    
+    public int minFunc(int x, int y) {
+        return x > y ? y : x;
+    }
+}
