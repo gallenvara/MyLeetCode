ad81a1337319113494cc3d26c69fc7886e96b406
gallenvara <gallenvara@126.com>
2016-04-24 21:15:42 +0800

    count bit from 0 to n

---
+++ CountingBits.java
@@ -0,0 +1,28 @@
+/*
+Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+
+Example:
+For num = 5 you should return [0,1,1,2,1,2].
+
+Follow up:
+
+It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+Space complexity should be O(n).
+Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+Hint:
+
+You should make use of what you have produced already.
+Divide the numbers in ranges like [2-3], [4-7], [8-15] and so on. And try to generate new range from previous.
+Or does the odd/even status of the number help you in calculating the number of 1s?
+*/
+
+public class Solution {
+    public int[] countBits(int num) {
+        int[] a = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+             int tmp = i - ((i >>1) &033333333333) - ((i >>2) &011111111111);
+             a[i] = ((tmp + (tmp >>3)) &030707070707) %63;
+        }
+        return a;
+    }
+}
